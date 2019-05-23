0ab4b03190b1d82bd037cd19d995778b0396d067
Johannes Winkelmann <jwi@sensirion.com>
2019-05-23 10:39:02 -0700

    split up MasterWrite and MasterRead for SHT

--- files/common/objects/essi2c.c
+++ files/common/objects/essi2c.c
@@ -59,14 +59,25 @@ ATMO_ESSI2C_Status_t ATMO_ESSI2C_GetTemperature(int32_t *temperatureCelsius)
 
     uint8_t data[ESS_SHT_DATA_LENGTH] = { 0 };
     uint8_t cmd[ESS_SHT_CMD_LENGTH] = { 0x78, 0x66 };
-    ATMO_I2C_Status_t readStatus = ATMO_I2C_MasterRead(_ATMO_ESSI2C_config.config.i2cDriverInstance, ESS_SHT_ADDR,
-                                                       cmd, ESS_SHT_CMD_LENGTH, data, ESS_SHT_DATA_LENGTH, 0);
+    ATMO_I2C_Status_t readStatus;
+    readStatus = ATMO_I2C_MasterWrite(_ATMO_ESSI2C_config.config.i2cDriverInstance,
+                                      ESS_SHT_ADDR, cmd, ESS_SHT_CMD_LENGTH,
+                                      NULL, 0,
+                                      0);
+    if (readStatus != ATMO_I2C_Status_Success) {
+        return ATMO_ESSI2C_Status_Fail;
+    }
+
+    ATMO_DelayMillisecondsNonBlock(100);
+
+    readStatus = ATMO_I2C_MasterRead(_ATMO_ESSI2C_config.config.i2cDriverInstance,
+                                    ESS_SHT_ADDR, NULL, 0, data, ESS_SHT_DATA_LENGTH, 0);
     if (readStatus != ATMO_I2C_Status_Success) {
         return ATMO_ESSI2C_Status_Fail;
     }
 
     //  mTemperature = mA + mB * (val / mC);
-    uint16_t rawTemp = (data[1] << 8) | data[0];
+    uint16_t rawTemp = (data[0] << 8) | data[1];
     *temperatureCelsius = (uint32_t)(-45 + (175.0 * rawTemp / 65535.0));
 
     return ATMO_ESSI2C_Status_Success;
